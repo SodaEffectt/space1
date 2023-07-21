0b4ba0adf4b6e8908ad23e6c6ef3d56a49548273
Dmytro <104826699+milinchukdmytro@users.noreply.github.com>
2023-07-21 14:19:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,23 +6,16 @@ pygame.font.init()
 
 WIDTH, HEIGHT = 750, 750
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Space Shooter Tutorial")
+pygame.display.set_caption("Space Invaders")
 
-# Load images
 RED_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
 GREEN_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_green_small.png"))
 BLUE_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_blue_small.png"))
-
-# Player player
 YELLOW_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_yellow.png"))
-
-# Lasers
 RED_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_red.png"))
 GREEN_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_green.png"))
 BLUE_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_blue.png"))
 YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
-
-# Background
 BG = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png")), (WIDTH, HEIGHT))
 
 
@@ -178,7 +171,6 @@ def main():
 
     def redraw_window():
         WIN.blit(BG, (0, 0))
-        # draw text
         lives_label = main_font.render(f"Lives: {lives}", True, (255, 255, 255))
         level_label = main_font.render(f"Level: {level}", True, (255, 255, 255))
 
@@ -223,13 +215,13 @@ def main():
                 quit()
 
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_a] and player.x - player_vel > 0:  # left
+        if keys[pygame.K_a] and player.x - player_vel > 0: 
             player.x -= player_vel
-        if keys[pygame.K_d] and player.x + player_vel + player.get_width() < WIDTH:  # right
+        if keys[pygame.K_d] and player.x + player_vel + player.get_width() < WIDTH: 
             player.x += player_vel
-        if keys[pygame.K_w] and player.y - player_vel > 0:  # up
+        if keys[pygame.K_w] and player.y - player_vel > 0: 
             player.y -= player_vel
-        if keys[pygame.K_s] and player.y + player_vel + player.get_height() + 15 < HEIGHT:  # down
+        if keys[pygame.K_s] and player.y + player_vel + player.get_height() + 15 < HEIGHT:  
             player.y += player_vel
         if keys[pygame.K_SPACE]:
             player.shoot()
@@ -256,7 +248,7 @@ def main_menu():
     run = True
     while run:
         WIN.blit(BG, (0, 0))
-        title_label = title_font.render("Press the mouse to begin...", True, (255, 255, 255))
+        title_label = title_font.render("Press the mouse to begin", True, (255, 255, 255))
         WIN.blit(title_label, (WIDTH / 2 - title_label.get_width() / 2, 350))
         pygame.display.update()
         for event in pygame.event.get():
